aacb60ba70e5f03267aef0869e8803580bf5bcf2
JulianMiranda <jmirandauria@gmail.com>
2024-08-31 09:43:21 -0500

    Succes

---
+++ src/Success.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import loading from './assets/loading.gif';
+
+export const Success = () => {
+	return (
+		<div style={{margin: 20}}>
+			<img src={loading} alt="loading..." />
+		</div>
+	);
+};
